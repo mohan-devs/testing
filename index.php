2ab6328ca4cd85a1b64f3daafcf4d3cdc1c756f6
nitishpatidar2311 <nitish.synetalsolutions@gmail.com>
2023-08-21 11:59:45 +0530

    test commit-1

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 // $googleDriveLink="https://drive.google.com/file/d/1GOf79Vwmj-tgZ1LyLod5-YuYPN5r7fVF/view";
 // $googleDriveLink="https://drive.google.com/drive/folders/1yRJ8Jja7r88_QAVL7tPSHt9aNxNdhE-N";
 // $googleDriveLink="https://drive.google.com/drive/folders/12NRl8-Dqn2EwNeqrRFVc4DDhcQsXpQtn";
-$googleDriveLink="https://drive.google.com/file/d/1tmZJ4SeX5W0wlJ1_2yF476eEopmJlpsb/view?usp=drivesdk";
+$googleDriveLink = "https://drive.google.com/file/d/1tmZJ4SeX5W0wlJ1_2yF476eEopmJlpsb/view?usp=drivesdk";
 
 // Extract file ID using string manipulation
 // $startPosition = strpos($googleDriveLink, '/d/') + 3;
@@ -19,12 +19,12 @@ $patterns = [
 
 foreach ($patterns as $pattern) {
     if (preg_match($pattern, $googleDriveLink, $matches)) {
-        $fileId= $matches[1];
+        $fileId = $matches[1];
     }
 }
 // echo "File ID: $fileId";
-$fileId="64d10aa2f9ebaa54b6d8f45e";
-$fileUrl = 'https://drive.google.com/uc?id='.$fileId;
+$fileId = "64d10aa2f9ebaa54b6d8f45e";
+$fileUrl = 'https://drive.google.com/uc?id=' . $fileId;
 
 // Download the file
 $fileContent = file_get_contents($fileUrl);
@@ -35,7 +35,7 @@ if (!is_dir(dirname($localFilePath))) {
 }
 
 // Save the downloaded file content to a local location
-$data=file_put_contents($localFilePath, $fileContent);
+$data = file_put_contents($localFilePath, $fileContent);
 
 
 // Download the file using cURL
@@ -48,7 +48,9 @@ $data=file_put_contents($localFilePath, $fileContent);
 // fclose($fp);
 
 // if($data){
-    // echo "File downloaded and saved successfully!";
+// echo "File downloaded and saved successfully!";
 // }
 ?>
 <!-- https://drive.google.com/uc?id=1XtC3RDoD24QNgYjmEW0WT-sRPhXIaXYy&export=download -->
+
+<!-- test commit 1 -->
